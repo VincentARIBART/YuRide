75beff6e74d94b6c8a8ae084051e3c77f976ffba
Vincent <vincent.aribart@gmail.com>
2019-09-15 13:59:28 +0200

    Create depo

---
+++ src/js/main.js
@@ -0,0 +1,29 @@
+import '../scss/main.scss';
+
+
+import { TweenLite, TimelineLite } from 'gsap';
+
+
+import win from './utils/Window.js';
+import io from './utils/Io.js';
+import scroll from './utils/Scroll.js';
+import fallback from './utils/Fallback.js';
+
+const loadHandler = () => {
+    if (document.readyState !== 'complete') return;
+    scroll.init();
+    win.setNoTransitionElts([...document.getElementsByClassName('element-without-transition-on-resize')]);
+    win.init();
+    io.init();
+    fallback.init();
+
+    // here
+
+
+}
+
+if (document.readyState === 'complete') {
+   loadHandler();
+} else {
+   $(window).on('load', loadHandler);
+}
